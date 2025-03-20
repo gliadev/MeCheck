2a48eacff5f443bf79879c2468204c9df1c13932
gliadev <gliatem@gmail.com>
2025-03-20 23:07:03 +0100

    Initial Commit

---
+++ MeCheck/MeCheckApp.swift
@@ -0,0 +1,32 @@
+//
+//  MeCheckApp.swift
+//  MeCheck
+//
+//  Created by Adolfo on 20/3/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MeCheckApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ MeCheckTests/MeCheckTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeCheckTests.swift
+//  MeCheckTests
+//
+//  Created by Adolfo on 20/3/25.
+//
+
+import Testing
+@testable import MeCheck
+
+struct MeCheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
